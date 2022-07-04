4a380eb04957e25a67eac7711990be8c6ca32e6d
bitencourt-eduardo <103838200+bitencourt-eduardo@users.noreply.github.com>
2022-07-04 00:08:06 -0300

    ok

---
+++ Desafio_01.py
@@ -0,0 +1,12 @@
+#Desafio 1
+#Escreva um programa em Python para contabilizar a quantidade de ocorrências de cada palavra.
+
+palavras = [
+   'red', 'green', 'black', 'pink', 'black', 'white', 'black', 'eyes',
+   'white', 'black', 'orange', 'pink', 'pink', 'red', 'red', 'white', 'orange',
+   'white', "black", 'pink', 'green', 'green', 'pink', 'green', 'pink',
+   'white', 'orange', "orange", 'red'
+]
+
+
+#
